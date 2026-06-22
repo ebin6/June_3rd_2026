015f02272c813217d355562966562be4fd259492
ebin6 <ebin.oneteam@gmail.com>
2026-06-22 10:19:33 +0530

    Python Function (*args , **kwargs )

--- function_pgm.py
+++ function_pgm.py
@@ -1,3 +1,4 @@
+'''
 def greet(name,age,place="Kochi",course=None):
     print(f"Hello {name} you are {age} years old")
    
@@ -5,3 +6,54 @@ def greet(name,age,place="Kochi",course=None):
 
 greet(name="Aswin",age=24,place='Kottayam')
 greet(age=29,name="Joyal")
+
+'''
+
+'''
+*args
+def add(*args):
+    total=0
+    for i in args:
+        total+=i
+    print(total)
+
+add(34,12)
+add(9,12,46)
+'''
+
+'''
+def greet(**kwargs):
+    print(f"Hello {kwargs["name"]} you are {kwargs["age"]} years old")
+
+greet(name="Aswin",age=24,place='Kottayam')
+
+'''
+
+
+'''def add(*args):
+    total=0
+    for i in args:
+        total+=i
+    return total
+
+s=add(34,12)
+print(s)'''
+
+'''def fact(num):
+    if num==1:
+        return num
+    else:
+        return num*fact(num-1)
+    
+print("Factrorial of 5 is : ",fact(5))'''
+'''
+def add(a,b):
+    return a+b
+print(add(34,12))
+'''
+add=lambda a,b:a+b
+
+print(add(31,5))
+
+is_even = lambda num: num%2==0 
+print(is_even(13))
